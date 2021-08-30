f13895e3dd902e8a208e47cc6f5e79dd78553e86
sz0tu <marcinszotu@gmail.com>
2021-08-30 14:32:36 +0200

    Init structure and settings project

---
+++ app.js
@@ -0,0 +1,52 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const httpErrors = require('http-errors');
+const logger = require('morgan');const nunjucks = require('nunjucks');
+const bodyParser = require("body-parser");
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'njk');
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+  next(httpErrors(404));
+});
+
+app.use((err, req, res, next) => {
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
